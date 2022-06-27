bb26f43fe6d22a64db02e653d9930e9e744f0cfb
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-06-28 01:48:11 +0530

    updated on 28/6/2022 at 2am

---
+++ NodeJS_CodeWithHarry/http_module.js
@@ -0,0 +1,33 @@
+import  http from 'http'
+// console.log(http.ServerResponse.prototype);
+const server = http.createServer();
+server.on("request", (request, response) => {
+    console.log(request.url);
+    response.setHeader('content-type', 'text/html');
+    if (request.url == '/') {
+        response.end("<h1>u r in homepage</h1>")
+    }
+    else {
+        if (request.url == '/city/') {
+            console.log(response.statusCode);
+            response.end('<h3>hello <h1>agartala</h1></h3 > ')
+        }
+        else {
+            response.statusCode = 404;
+            response.end('<h1>not found</h1>');           
+        }
+        
+    }
+    server.close((err) => {
+            console.log("server is closed bye bye");
+        
+        })
+})
+
+server.listen(process.env.PORT||8000, () => {
+    console.log( "listening at 8000" );    
+})
+ 
+
+//https://stackoverflow.com/questions/41137902/how-does-server-listen-keep-the-node-program-running
+
