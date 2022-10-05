5661b1ac19e8692fc9c233aa88299536f2031e26
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-06 00:38:13 +0530

    some simple code updates

--- Codding_Addict/http/app.js
+++ Codding_Addict/http/app.js
@@ -1,50 +1,54 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const homePage = fs.readFileSync('./navbar-app/index.html');
-const homeStyles = fs.readFileSync('./navbar-app/styles.css');
-const homeImage = fs.readFileSync('./navbar-app/logo.svg');
-const homeLogic = fs.readFileSync('./navbar-app/browser-app.js');
+const homePage = fs.readFileSync("./navbar-app/index.html");
+const homeStyles = fs.readFileSync("./navbar-app/styles.css");
+const homeImage = fs.readFileSync("./navbar-app/logo.svg");
+const homeLogic = fs.readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
+  const url = req.url;
+
   // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
   }
+
   // about page
-//   else if (url === '/about') {
-//     res.writeHead(200, { 'content-type': 'text/html' })
-//     res.write('<h1>about page</h1>')
-//     res.end()
-//   }
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
+
   // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
   }
+
   // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+    res.end();
   }
+
   // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
+    res.end();
+  } 
+    
   // 404
   else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>page not found</h1>");
+    res.end();
   }
-})
+});
 server.listen(5000);
