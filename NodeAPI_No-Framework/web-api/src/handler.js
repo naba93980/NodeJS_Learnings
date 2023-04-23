a6d21c132687c2315efebcd93f6e01dfbe23083a
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-04-23 23:55:11 +0530

    handles req, calls controller, handles error thrown from controller

---
+++ NodeAPI_No-Framework/web-api/src/handler.js
@@ -0,0 +1,51 @@
+import { parse } from "node:url";
+import { DEFAULT_HEADER } from "./util/util.js";
+
+const allRoutes = {
+
+    '/heroes:get': (request, response) => {
+        throw new Error('tessssst')
+        response.write('GET')
+        response.end()
+    },
+
+    // 404 routes
+    default: (request, response) => {
+        response.writeHead(404, DEFAULT_HEADER)
+        response.write('Not Found!')
+        response.end()
+    }
+}
+
+// 1 request handler
+function handler(req, res) {
+    const {
+        url,
+        method
+    } = req;
+
+    const {
+        pathname
+    } = parse(url);
+
+    const key = `${pathname}:${method.toLowerCase()}`
+    const chosen = allRoutes[key] || allRoutes.default              // 2 decides which controller to call
+    
+    return new Promise((resolve, reject) => resolve(chosen(req, res))).catch(handlerError(res))       // 3 async handler returns promise, then runs the controller through the promise executor
+
+}
+
+// 4 error handler to handle any async error from controllers
+function handlerError(response) {
+    return error => {
+        console.log('Something bad has  happened**', error.stack)
+        response.writeHead(500, DEFAULT_HEADER)
+        response.write(JSON.stringify({
+            error: 'internet server error!!'
+        }))
+
+        return response.end()
+    }
+}
+
+export default handler;
